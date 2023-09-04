ca7f4fc7ff16c6f2b237ffbb7cd3c8de74dd7141
Patrik Modesto <mody@centrum.cz>
2023-09-04 16:48:13 +0200

    day6 - part 1

---
+++ src/day6.cc
@@ -0,0 +1,42 @@
+#include <fmt/core.h>
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using Freq = std::map<unsigned char, unsigned>;
+
+int main()
+{
+    std::vector<Freq> data;
+
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        if(line.empty()) {
+            break;
+        }
+        if(data.size() < line.size()) {
+            data.resize(line.size());
+        }
+
+        for(unsigned i = 0; i < line.size(); ++i) {
+            Freq& f = data.at(i);
+            f.insert({line.at(i), 0}).first->second++;
+        }
+    }
+
+    std::string result1;
+
+    for (auto const& f : data) {
+        auto it = std::max_element(
+            f.cbegin(), f.cend(), [](auto const& lhs, auto const& rhs) { return lhs.second < rhs.second; });
+        assert(it != f.cend());
+        result1 += it->first;
+    }
+
+    fmt::print("1: {}\n", result1);
+
+    return 0;
+}
