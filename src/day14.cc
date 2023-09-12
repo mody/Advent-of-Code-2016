ef0b0592d2c9b5478acd2fabb0b422a5a2c61b3a
Patrik Modesto <mody@centrum.cz>
2023-09-12 11:42:44 +0200

    day14 - part 2

--- src/day14.cc
+++ src/day14.cc
@@ -1,8 +1,12 @@
 #define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
-#include <cryptopp/md5.h>
 #include <cryptopp/hex.h>
+#include <cryptopp/md5.h>
 
 #include <fmt/core.h>
+
+#include <oneapi/tbb/blocked_range.h>
+#include <oneapi/tbb/concurrent_map.h>
+#include <oneapi/tbb/parallel_for.h>
 
 #include <cassert>
 #include <iterator>
@@ -25,84 +29,80 @@ std::string find_key(const unsigned size, const std::string_view input)
             ++count;
         }
         if (count == size) {
-            // if (i == (input.size() - 1) || c != input.at(i + 1)) {
-                return std::string(count, c);
-            // }
+            return std::string(count, c);
         }
     }
     return {};
 }
 
-int main()
+unsigned process(unsigned stretch)
 {
-    std::map<unsigned, std::string> three_pos;
-    std::multimap<std::string, unsigned> five_pos;
+    oneapi::tbb::concurrent_map<unsigned, std::string> three_pos;
+    oneapi::tbb::concurrent_multimap<std::string, unsigned> five_pos;
 
-    // for (unsigned i = 0; i < std::numeric_limits<unsigned>::max(); ++i) {
-    for (unsigned i = 0; i < 50000; ++i) {
-        const std::string msg = "ngcjuoqr" + std::to_string(i);
-        // const std::string msg = "abc" + std::to_string(i);
+    oneapi::tbb::parallel_for(
+        oneapi::tbb::blocked_range<unsigned>(0, 30000),
+        [&three_pos, &five_pos, &stretch](oneapi::tbb::blocked_range<unsigned> const& r) {
+            CryptoPP::Weak::MD5 md5;
+            std::string decoded;
+            decoded.resize(md5.DigestSize());
 
-        CryptoPP::Weak::MD5 md5;
-        md5.Update((const byte*)&msg[0], msg.size());
-        std::string decoded;
-        decoded.resize(md5.DigestSize());
-        md5.Final((byte*)&decoded[0]);
+            for (unsigned i = r.begin(); i < r.end(); ++i) {
+                std::string msg = "ngcjuoqr" + std::to_string(i);
 
-        CryptoPP::HexEncoder encoder(nullptr, false);
-        encoder.Put((byte*)decoded.data(), decoded.size());
-        encoder.MessageEnd();
+                for (unsigned j = 0; j <= stretch; ++j) {
+                    md5.Update((const byte*)&msg[0], msg.size());
+                    md5.Final((byte*)&decoded[0]);
 
-        std::string encoded;
-        const auto size = encoder.MaxRetrievable();
-        if (size) {
-            encoded.resize(size);
-            encoder.Get((byte*)&encoded[0], encoded.size());
-        }
+                    CryptoPP::HexEncoder encoder(nullptr, false);
+                    encoder.Put((byte*)decoded.data(), decoded.size());
+                    encoder.MessageEnd();
 
-        // fmt::print ("{}\t{} ", i, encoded);
+                    const auto size = encoder.MaxRetrievable();
+                    if (size) {
+                        msg.resize(size);
+                        encoder.Get((byte*)&msg[0], msg.size());
+                    }
+                }
 
-        std::string key = find_key(5, encoded);
-        if (key.size() == 5) {
-            // fmt::print("found {}", key);
-            five_pos.insert({key, i});
-            // fmt::print("\n");
-        }
+                std::string key = find_key(5, msg);
+                if (key.size() == 5) {
+                    five_pos.insert({key, i});
+                }
 
-        key = (key.size() == 5) ? key.substr(0, 3) : find_key(3, encoded);
-        if (key.size() == 3) {
-            // fmt::print("found {}", key);
-            three_pos.insert({i, key});
-        }
-
-        // fmt::print("\n");
-    }
+                key = (key.size() == 5) ? key.substr(0, 3) : find_key(3, msg);
+                if (key.size() == 3) {
+                    three_pos.insert({i, key});
+                }
+            }
+        });
 
     unsigned num_keys = 0;
-    unsigned result1 = 0;
+    unsigned result = 0;
 
     for (auto const& [i, key3] : three_pos) {
         const std::string key5 = std::string(5, key3.at(0));
-        // fmt::print("{} at {} ", key3, i);
         for (auto it5 = five_pos.lower_bound(key5); it5 != five_pos.end() && it5->first == key5; ++it5) {
-            // fmt::print(", key5 at {}", it5->second);
             const auto diff = (it5->second - i);
             if (diff > 0 && diff < 1000) {
                 ++num_keys;
-                // fmt::print(" {} within limit, keys {}", diff, num_keys);
                 if (num_keys == 64) {
-                    result1 = i;
+                    result = i;
                 }
                 break;
             }
         }
-        // fmt::print("\n");
         if (num_keys == 64) {
             break;
         }
     }
+    return result;
+}
 
-    fmt::print("1: {}\n", result1);
+int main()
+{
+    fmt::print("1: {}\n", process(0));
+    fmt::print("2: {}\n", process(2016));
 
     return 0;
 }
