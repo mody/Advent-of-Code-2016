04433de239df217e5929f4cfaa8ddd00f36fb060
Patrik Modesto <mody@centrum.cz>
2023-09-13 10:29:22 +0200

    day15 - part 2

--- src/day15.cc
+++ src/day15.cc
@@ -3,6 +3,7 @@
 #include <boost/algorithm/string/split.hpp>
 #include <fmt/core.h>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <vector>
 
@@ -12,11 +13,30 @@ struct Wheel
     unsigned offset = 0;
 };
 
+using Wheels = std::vector<Wheel>;
 
+
+unsigned find_time(Wheels wheels)
+{
+    const unsigned max_time = std::accumulate(
+        wheels.cbegin(), wheels.cend(), 1, [](unsigned const& a, Wheel const& w2) { return a * w2.positions; });
+
+    for(unsigned t = 0; t < max_time; ++t) {
+        std::vector<unsigned> positions(wheels.size(), 0u);
+        for (unsigned i = 0; i < wheels.size(); ++i) {
+            positions.at(i) = (wheels.at(i).offset + t + i + 1) % wheels.at(i).positions;
+        }
+
+        if (std::all_of(positions.cbegin(), positions.cend(), [](unsigned const& v) { return v == 0; })) {
+            return t;
+        }
+    }
+    return 0;
+}
 
 int main()
 {
-    std::vector<Wheel> wheels;
+    Wheels wheels;
 
     std::string line;
     while(std::getline(std::cin, line)) {
@@ -30,52 +50,11 @@ int main()
         wheels.emplace_back(std::move(w));
     }
 
-    for(unsigned t = 0; t < 500000; ++t) {
-        std::vector<unsigned> positions(wheels.size(), 0u);
-        for (unsigned i = 0; i < wheels.size(); ++i) {
-            positions.at(i) = (wheels.at(i).offset + t + i + 1) % wheels.at(i).positions;
-        }
+    fmt::print("1: {}\n", find_time(wheels));
 
-        if (std::all_of(positions.cbegin(), positions.cend(), [](unsigned const& v) { return v == 0; })) {
-            fmt::print("1: {}\n", t);
-            break;
-        }
-    }
+    wheels.push_back(Wheel{.positions = 11});
+
+    fmt::print("2: {}\n", find_time(wheels));
 
     return 0;
 }
-
-#if 0
-
-w1_max = 5
-w1_start = 4
-
-w1_pos = (w1_start + (t % w1_max)) % w1_max
-
-Time    w1      w2
- 0	4	1
- 1	0	0
- 2	1	1
- 3	2	0
- 4	3	1
- 5	4	0
- 6	0	1
- 7	1	0
- 8	2	1
- 9	3	0
-10	4	1
-11	0	0
-12	1	1
-13	2	0
-14	3	1
-15	4	0
-16	0	1
-17	1	0
-18	2	1
-19	3	0
-20	4	1
-
-
-w1_pos = (w1_start + t) % w1_max
-
-#endif
