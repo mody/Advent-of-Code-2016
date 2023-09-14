845450dc2676119f98f76db289c8cb39bd7772f0
Patrik Modesto <mody@centrum.cz>
2023-09-14 21:02:44 +0200

    day19 - part 2

--- src/day19.cc
+++ src/day19.cc
@@ -3,48 +3,36 @@
 
 int main()
 {
-    // for (unsigned i = 2; i < 17; ++i) {
-    //     unsigned res = 1 + 2 * (i - (unsigned)std::exp2((unsigned)std::log2(i)));
-    //     fmt::print("{} -> {}\n", i, res);
-    // }
+    const unsigned INPUT = 3004953;
+    {
+        const unsigned res = 1 + 2 * (INPUT - (unsigned)std::pow(2, (unsigned)std::log2(INPUT)));
+        fmt::print("1: {}\n", res);
+    }
 
-    const unsigned i = 3004953;
-    const unsigned res = 1 + 2 * (i - (unsigned)std::exp2((unsigned)std::log2(i)));
-    fmt::print("1: {}\n", res);
+    // see https://oeis.org/A334473
+    auto highest_power_of_3 = [](unsigned n) -> unsigned {
+        unsigned val = 0;
+        for (; static_cast<unsigned>(std::pow(3, val)) <= n; ++val)
+            ;
+        return static_cast<unsigned>(std::pow(3, val - 1));
+    };
+
+    auto solve_shooters = [&highest_power_of_3](unsigned n) -> unsigned {
+        const unsigned x = highest_power_of_3(n);
+        if (x == n) {
+            return x;
+        } else {
+            if (n < 2 * x) {
+                return n % x;
+            } else {
+                return x + 2 * (n % x);
+            }
+        }
+        return 0;
+    };
+
+    fmt::print("2: {}\n", solve_shooters(INPUT));
 
     return 0;
 }
 
-#if 0
-
-1 x (2)
-
-x x 3 (3)
-
-1 x x x (4)
-
-x x 3 x x (5)
-
-x x x x 5 x (6)
-
-x x x x x x 7 (7)
-
-1 x x x x x x x (8)
-
-x x 3 x x x x x x (9)
-
-x x x x 5 x x x x x (10)
-
-x x x x x x 7 x x x x (11)
-
-x x x x x x x x 9 x x x (12)
-
-x x x x x x x x x x b x x (13)
-
-x x x x x x x x x x x x d x (14)
-
-x x x x x x x x x x x x x x f (15)
-
-1 x x x x x x x x x x x x x x x (16)
-
-#endif
